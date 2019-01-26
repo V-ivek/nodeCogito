6a6450960fe15849f1ec5c6d9da1623263c1bfd7
Vivek Selvarajan <agent47vivek@gmail.com>
2019-01-26 14:10:28 +0530

    Heroku support added

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 
+const port = process.env.PORT || 3000
 var app = express();
 
 app.get('/',(req,res) => {  
@@ -7,6 +8,6 @@ app.get('/',(req,res) => {
 });
 
 
-app.listen(3000, () => {
-    console.log("server listening at port 3000");
+app.listen(port, () => {
+    console.log(`server listening at port ${port}`);
 })
