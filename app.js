ca50ab415b48094d31989be6d96663f5c12fa15b
Vivek Selvarajan <agent47vivek@gmail.com>
2019-01-26 13:18:36 +0530

    Initialize

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require('express');
+
+var app = express();
+
+app.get('/',(req,res) => {  
+    res.send("Hello world");
+});
+
+
+app.listen(3000, () => {
+    console.log("server listening at port 3000");
+})
